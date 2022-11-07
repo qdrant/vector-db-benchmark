8a8031e3a0e79d2056d172b24d66dc6070fad4fe
Kacper Łukawski <lukawski.kacper@gmail.com>
2022-11-07 13:39:59 +0100

    Enhance run.py with timeout and multiple engines/datasets

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import fnmatch
+import traceback
+from typing import List
 
+import stopit
 import typer
 
 from benchmark.config_read import read_dataset_config, read_engine_configs
@@ -12,14 +15,16 @@ app = typer.Typer()
 
 @app.command()
 def run(
-    engines: str = "*",
-    datasets: str = "*",
+    engines: List[str] = typer.Option(["*"]),
+    datasets: List[str] = typer.Option(["*"]),
     host: str = "localhost",
     skip_upload: bool = False,
+    exit_on_error: bool = True,
+    timeout: float = 86400.0,
 ):
     """
     Example:
-        python3 run --engines *-m-16-* --datasets glove-*
+        python3 run --engines *-m-16-* --engines qdrant-* --datasets glove-*
     """
     all_engines = read_engine_configs()
     all_datasets = read_dataset_config()
@@ -27,12 +32,12 @@ def run(
     selected_engines = {
         name: config
         for name, config in all_engines.items()
-        if fnmatch.fnmatch(name, engines)
+        if any(fnmatch.fnmatch(name, engine) for engine in engines)
     }
     selected_datasets = {
         name: config
         for name, config in all_datasets.items()
-        if fnmatch.fnmatch(name, datasets)
+        if any(fnmatch.fnmatch(name, dataset) for dataset in datasets)
     }
 
     for engine_name, engine_config in selected_engines.items():
@@ -42,9 +47,30 @@ def run(
             dataset = Dataset(dataset_config)
             dataset.download()
             try:
-                client.run_experiment(dataset, skip_upload)
+                with stopit.ThreadingTimeout(timeout) as tt:
+                    client.run_experiment(dataset, skip_upload)
+
+                # If the timeout is reached, the server might be still in the
+                # middle of some background processing, like creating the index.
+                # Next experiment should not be launched. It's better to reset
+                # the server state manually.
+                if tt.state != stopit.ThreadingTimeout.EXECUTED:
+                    print(
+                        f"Timed out {engine_name} - {dataset_name}, "
+                        f"exceeded {timeout} seconds"
+                    )
+                    exit(2)
             except IncompatibilityError as e:
                 print(f"Skipping {engine_name} - {dataset_name}, incompatible params")
+                continue
+            except KeyboardInterrupt as e:
+                traceback.print_exc()
+                exit(1)
+            except Exception as e:
+                print(f"Experiment {engine_name} - {dataset_name} interrupted")
+                traceback.print_exc()
+                if exit_on_error:
+                    raise e
                 continue
 
 
