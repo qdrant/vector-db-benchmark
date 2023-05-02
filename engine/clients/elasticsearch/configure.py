a43e4e5f617c2b81cb952dcdc2235de65911329a
Michael Bischoff <michael.bischoff@elastic.co>
2023-05-03 01:31:49 +0200

    - set refresh_interval to -1 to speed up indexing - exclude vector from source

--- engine/clients/elasticsearch/configure.py
+++ engine/clients/elasticsearch/configure.py
@@ -55,7 +55,19 @@ class ElasticConfigurator(BaseConfigurator):
 
         self.client.indices.create(
             index=ELASTIC_INDEX,
+            settings={
+                "index": {
+                    "number_of_shards": 1,
+                    "number_of_replicas": 0,
+                    "refresh_interval": -1,
+                }
+            },
             mappings={
+                "_source": {
+                    "excludes": [
+                        "vector"
+                    ]
+                },
                 "properties": {
                     "vector": {
                         "type": "dense_vector",
