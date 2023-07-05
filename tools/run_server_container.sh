97c2551587169dfec109d527a5c127e42b940e3c
generall <andrey@vasnetsov.com>
2023-07-05 16:18:49 +0200

    allow qdrant version selection

--- tools/run_server_container.sh
+++ tools/run_server_container.sh
@@ -14,11 +14,13 @@ SCRIPT_PATH=$(dirname "$SCRIPT")
 
 BENCH_SERVER_NAME=${SERVER_NAME:-"benchmark-server-1"}
 
+QDRANT_VERSION=${QDRANT_VERSION:-"dev"}
+
 IP_OF_THE_SERVER=$(bash "${SCRIPT_PATH}/${CLOUD_NAME}/get_public_ip.sh" "$BENCH_SERVER_NAME")
 
 bash -x "${SCRIPT_PATH}/sync_servers.sh" "root@$IP_OF_THE_SERVER"
 
-DOCKER_COMPOSE="docker compose down ; docker compose up -d"
+DOCKER_COMPOSE="export QDRANT_VERSION=${QDRANT_VERSION} ; docker compose down ; docker compose up -d"
 
 ssh -t "${SERVER_USERNAME}@${IP_OF_THE_SERVER}" "cd ./projects/vector-db-benchmark/engine/servers/${CONTAINER_NAME} ; $DOCKER_COMPOSE"
 
