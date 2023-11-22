a4d035d7d4b79a092239e247ce0760501d90259b
filipecosta90 <filipecosta.90@gmail.com>
2023-11-22 23:44:50 +0000

    Removed spurious REDIS_HYBRID_POLICY env var

--- engine/clients/redis/config.py
+++ engine/clients/redis/config.py
@@ -4,7 +4,6 @@ REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_AUTH = os.getenv("REDIS_AUTH", None)
 REDIS_USER = os.getenv("REDIS_USER", None)
 REDIS_CLUSTER = bool(int(os.getenv("REDIS_CLUSTER", 0)))
-REDIS_HYBRID_POLICY = os.getenv("REDIS_HYBRID_POLICY", None)
 
 # 90 seconds timeout
 REDIS_QUERY_TIMEOUT = int(os.getenv("REDIS_QUERY_TIMEOUT", 90 * 1000))
