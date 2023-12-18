7aae1b2b98d4d341a2c711e37ae066ebdf6bd9ce
KShivendu <kshivendu1@gmail.com>
2023-12-18 21:11:52 +0530

    chores: debug

--- tools/qdrant_mem_usage.sh
+++ tools/qdrant_mem_usage.sh
@@ -20,4 +20,6 @@ MEMORY_USAGE=$(ssh -t "${SERVER_USERNAME}@${IP_OF_THE_SERVER}" "grep VmRSS /proc
 
 CURRENT_DATE=$(date +%Y-%m-%d-%H-%M-%S)
 
+ls $SCRIPT_PATH/results
+
 echo $MEMORY_USAGE > ${SCRIPT_PATH}/results/memory-usage-${CURRENT_DATE}.txt
