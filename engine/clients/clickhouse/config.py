5ca52a12e9448307f6ca262529960eb8c9a890f4
Dale Mcdiarmid <dale@clickhouse.com>
2024-01-10 13:12:49 +0000

    fix password

--- engine/clients/clickhouse/config.py
+++ engine/clients/clickhouse/config.py
@@ -4,7 +4,7 @@ CLICKHOUSE_PORT = 8123
 CLICKHOUSE_DATABASE = "default"
 CLICKHOUSE_TABLE = "bench"
 CLICKHOUSE_USER = "default"
-CLICKHOUSE_PASSWORD = ""
+CLICKHOUSE_PASSWORD = "passwd"
 DISTANCE_MAPPING = {
     Distance.L2: "L2Distance",
     Distance.COSINE: "cosineDistance",
