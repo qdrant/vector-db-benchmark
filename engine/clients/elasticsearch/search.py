8b324039ef4960b8d3af7d5044cc7e755869fd73
Dale Mcdiarmid <dale@clickhouse.com>
2024-01-11 18:17:11 +0000

    fix Elasticsearch precision

--- engine/clients/elasticsearch/search.py
+++ engine/clients/elasticsearch/search.py
@@ -66,7 +66,7 @@ class ElasticSearcher(BaseSearcher):
                         }
                     }
                 }
-            })
+            }, size=top)
 
         else:
             knn = {
