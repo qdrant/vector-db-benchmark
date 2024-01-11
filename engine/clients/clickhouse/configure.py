69eede397a8860a410bf3c83dc75308ad697e541
Dale Mcdiarmid <dale@clickhouse.com>
2024-01-11 18:44:23 +0000

    support hnsw for CH

--- engine/clients/clickhouse/configure.py
+++ engine/clients/clickhouse/configure.py
@@ -7,7 +7,7 @@ from engine.clients.clickhouse.config import (
     CLICKHOUSE_TABLE,
     CLICKHOUSE_USER,
     CLICKHOUSE_PORT,
-    CLICKHOUSE_PASSWORD, CLICKHOUSE_DATABASE,
+    CLICKHOUSE_PASSWORD, CLICKHOUSE_DATABASE, DISTANCE_MAPPING,
 )
 
 
@@ -24,8 +24,16 @@ class ClickHouseConfigurator(BaseConfigurator):
         super().__init__(host, collection_params, connection_params)
         self.client = clickhouse_connect.get_client(host=host, username=CLICKHOUSE_USER, password=CLICKHOUSE_PASSWORD,
                                                     database=CLICKHOUSE_DATABASE, port=CLICKHOUSE_PORT,
-                                                    **connection_params)
+                                                    settings={"allow_experimental_usearch_index": "1"},
+                                                             ** connection_params)
         self.engine = collection_params["engine"] if "engine" in collection_params else "MergeTree"
+        self.index = "exact"
+        self.index_params = {}
+        if "index" in collection_params:
+            if "type" in collection_params["index"]:
+                self.index = collection_params["index"]["type"]
+                self.index_params = collection_params["index"]["params"] if "params" in collection_params[
+                    "index"] else {}
 
     def clean(self):
         self.client.command(
@@ -39,10 +47,15 @@ class ClickHouseConfigurator(BaseConfigurator):
         order_by = ""
         if not self.engine == "Memory":
             order_by = "ORDER BY tuple()"
-        self.client.command(f"CREATE TABLE IF NOT EXISTS {CLICKHOUSE_TABLE} (id UInt32, vector Array(Float32), {','.join(columns)}) ENGINE = {self.engine} {order_by}")
+        index = ""
+        if self.index.lower() == "hnsw":
+            columns.append(f"INDEX hnsw_indx vector TYPE usearch('{DISTANCE_MAPPING[dataset.config.distance]}', 'f32')")
+        command = f"CREATE TABLE IF NOT EXISTS {CLICKHOUSE_TABLE} (id UInt32, vector Array(Float32), {','.join(columns)}) ENGINE = {self.engine} {order_by} {index}"
+
+        self.client.command(command)
 
     def _prepare_columns_config(self, dataset: Dataset):
         columns = []
         for field_name, field_type in dataset.config.schema.items():
-            columns.append(f"{field_name} { self.DTYPE_MAPPING.get(field_type, field_type) }")
+            columns.append(f"{field_name} {self.DTYPE_MAPPING.get(field_type, field_type)}")
         return columns
