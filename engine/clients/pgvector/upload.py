69068cd8722c6c49a140c7739f710909c026a4f5
Andrew Kane <andrew@ankane.org>
2024-01-30 18:07:23 -0800

    pgvector improvements

--- engine/clients/pgvector/upload.py
+++ engine/clients/pgvector/upload.py
@@ -4,11 +4,16 @@ import numpy as np
 import psycopg
 from pgvector.psycopg import register_vector
 
+from engine.base_client.distances import Distance
 from engine.base_client.upload import BaseUploader
 from engine.clients.pgvector.config import get_db_config
 
 
 class PgVectorUploader(BaseUploader):
+    DISTANCE_MAPPING = {
+        Distance.L2: "vector_l2_ops",
+        Distance.COSINE: "vector_cosine_ops",
+    }
     conn = None
     cur = None
     upload_params = {}
@@ -27,9 +32,27 @@ class PgVectorUploader(BaseUploader):
         vectors = np.array(vectors)
 
         # Copy is faster than insert
-        with cls.cur.copy("COPY items (id, embedding) FROM STDIN") as copy:
+        with cls.cur.copy("COPY items (id, embedding) FROM STDIN WITH (FORMAT BINARY)") as copy:
+            copy.set_types(["integer", "vector"])
             for i, embedding in zip(ids, vectors):
                 copy.write_row((i, embedding))
+
+    @classmethod
+    def post_upload(cls, distance):
+        try:
+            hnsw_distance_type = cls.DISTANCE_MAPPING[distance]
+        except KeyError:
+            raise IncompatibilityError(
+                f"Unsupported distance metric: {distance}"
+            )
+
+        cls.conn.execute("SET max_parallel_workers = 128")
+        cls.conn.execute("SET max_parallel_maintenance_workers = 128")
+        cls.conn.execute(
+            f"CREATE INDEX ON items USING hnsw (embedding {hnsw_distance_type}) WITH (m = {cls.upload_params['hnsw_config']['m']}, ef_construction = {cls.upload_params['hnsw_config']['ef_construct']})"
+        )
+
+        return {}
 
     @classmethod
     def delete_client(cls):
