a8d022ff00177eeccdd8a3625a10f8c07e473db4
filipecosta90 <filipecosta.90@gmail.com>
2024-02-07 17:46:46 -0500

    Enabled passing postgres connection details

--- engine/clients/pgvector/config.py
+++ engine/clients/pgvector/config.py
@@ -1,7 +1,9 @@
-PGVECTOR_PORT = 9200
-PGVECTOR_DB = "postgres"
-PGVECTOR_USER = "postgres"
-PGVECTOR_PASSWORD = "passwd"
+import os
+
+PGVECTOR_PORT = int(os.getenv("PGVECTOR_PORT", 9200))
+PGVECTOR_DB = os.getenv("PGVECTOR_DB", "postgres")
+PGVECTOR_USER = os.getenv("PGVECTOR_USER", "postgres")
+PGVECTOR_PASSWORD = os.getenv("PGVECTOR_PASSWORD", "passwd")
 
 
 def get_db_config(host, connection_params):
