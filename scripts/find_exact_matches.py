9b4c1145f12c2784bd1b9be6a00ae32f4b141a3d
Dale Mcdiarmid <dale@clickhouse.com>
2024-02-20 11:57:24 +0000

    scripts + qdrant on disk

--- scripts/find_exact_matches.py
+++ scripts/find_exact_matches.py
@@ -2,11 +2,12 @@ import json
 import clickhouse_connect
 
 
-client = clickhouse_connect.get_client(host='w60d4cvz06.eu-central-1.aws.clickhouse.cloud', port=8443, username='default', password='r89_1Hiyf~NgB')
+client = clickhouse_connect.get_client(host='localhost', port=8123, username='default', password='')
 
 with open('queries.jsonl', 'r') as queries_file, open('neighbours.jsonl', 'w') as neighbours_file:
     for line in queries_file:
         query = json.loads(line.strip())
-        result = client.query(f'SELECT id FROM hackernews_embeddings_annoy3 ORDER BY cosineDistance(vector, {query}) ASC LIMIT 100')
-        ids = [row[0] for row in result.result_rows]
+        result = client.query(f'SELECT id FROM bench ORDER BY cosineDistance(vector, {query}) ASC LIMIT 100')
+        ids = [int(row[0]) for row in result.result_rows]
         neighbours_file.write(json.dumps(ids)+"\n")
+        neighbours_file.flush()
