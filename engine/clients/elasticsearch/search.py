3f314d82659cac25d0c983161147706506061879
filipecosta90 <filipecosta.90@gmail.com>
2024-03-04 13:57:43 +0000

    popping the parallel config from a deep copy of search_params

--- engine/clients/elasticsearch/search.py
+++ engine/clients/elasticsearch/search.py
@@ -1,3 +1,4 @@
+import copy
 import multiprocessing as mp
 import uuid
 from typing import List, Tuple
@@ -34,7 +35,9 @@ class ElasticSearcher(BaseSearcher):
             **connection_params,
         }
         cls.client = get_es_client(host, connection_params)
-        cls.search_params = search_params
+        cls.search_params = copy.deepcopy(search_params)
+        # pop parallel
+        cls.search_params.pop("parallel", "1")
 
     @classmethod
     def search_one(cls, vector, meta_conditions, top) -> List[Tuple[int, float]]:
