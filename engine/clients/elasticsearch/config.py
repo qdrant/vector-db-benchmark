9751080444412920f2b918a0dd83d24b68209772
filipecosta90 <filipecosta.90@gmail.com>
2024-04-04 10:34:21 +0100

    Removed api_key usage per review

--- engine/clients/elasticsearch/config.py
+++ engine/clients/elasticsearch/config.py
@@ -6,7 +6,6 @@ ELASTIC_PORT = int(os.getenv("ELASTIC_PORT", 9200))
 ELASTIC_INDEX = os.getenv("ELASTIC_INDEX", "bench")
 ELASTIC_USER = os.getenv("ELASTIC_USER", "elastic")
 ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD", "passwd")
-ELASTIC_API_KEY = os.getenv("ELASTIC_API_KEY", None)
 ELASTIC_TIMEOUT = int(os.getenv("ELASTIC_TIMEOUT", 90))
 
 
@@ -22,7 +21,6 @@ def get_es_client(host, connection_params):
     else:
         url = "http://"
     url += f"{host}:{ELASTIC_PORT}"
-    if ELASTIC_API_KEY is None:
-        return Elasticsearch(url, basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD), **init_params)
-    else:
-        return Elasticsearch(url, api_key=ELASTIC_API_KEY, **init_params)
+    return Elasticsearch(
+        url, basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD), **init_params
+    )
