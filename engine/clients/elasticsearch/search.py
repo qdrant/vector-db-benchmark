5f2121e63508d009c9a0fe664406adf18fc15c1d
Kumar Shivendu <kshivendu1@gmail.com>
2024-04-16 00:18:29 +0530

    refactor: Nested search params in ES config (#120)
    * refactor: Nested search params in ES config
    
    Co-authored-by: filipe oliveira <filipecosta.90@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: Remove extra config vars
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: Remove extra comment
    
    * feat: Add keyword, text, and float index types in ES
    
    ---------
    
    Co-authored-by: filipe oliveira <filipecosta.90@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- engine/clients/elasticsearch/search.py
+++ engine/clients/elasticsearch/search.py
@@ -5,12 +5,7 @@ from typing import List, Tuple
 from elasticsearch import Elasticsearch
 
 from engine.base_client.search import BaseSearcher
-from engine.clients.elasticsearch.config import (
-    ELASTIC_INDEX,
-    ELASTIC_PASSWORD,
-    ELASTIC_PORT,
-    ELASTIC_USER,
-)
+from engine.clients.elasticsearch.config import ELASTIC_INDEX, get_es_client
 from engine.clients.elasticsearch.parser import ElasticConditionParser
 
 
@@ -29,20 +24,8 @@ class ElasticSearcher(BaseSearcher):
         return "forkserver" if "forkserver" in mp.get_all_start_methods() else "spawn"
 
     @classmethod
-    def init_client(cls, host, distance, connection_params: dict, search_params: dict):
-        init_params = {
-            **{
-                "verify_certs": False,
-                "request_timeout": 90,
-                "retry_on_timeout": True,
-            },
-            **connection_params,
-        }
-        cls.client: Elasticsearch = Elasticsearch(
-            f"http://{host}:{ELASTIC_PORT}",
-            basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD),
-            **init_params,
-        )
+    def init_client(cls, host, _distance, connection_params: dict, search_params: dict):
+        cls.client = get_es_client(host, connection_params)
         cls.search_params = search_params
 
     @classmethod
