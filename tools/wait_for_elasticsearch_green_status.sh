f7a47391e6bb5d418c59060b62cdd6907f7e0a10
tellet-q <elena.dubrovina@qdrant.com>
2024-04-17 15:47:30 +0200

    Introduce waits

---
+++ tools/wait_for_elasticsearch_green_status.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+ES_HOST=${1:-"localhost:9200"}
+
+until $(curl --output /dev/null --silent --head --fail "$ES_HOST"); do
+    printf '.'
+    sleep 1
+done
+
+# Wait for ES to start...
+response=$(curl "$ES_HOST")
+until [ "$response" = "200" ]; do
+    response=$(curl --write-out %{http_code} --silent --output /dev/null "$ES_HOST")
+    >&2 echo "Elastic Search is unavailable - sleeping"
+    sleep 1
+done
+
+# Wait for ES status to turn to Green
+health="$(curl -fsSL "$ES_HOST/_cat/health?h=status")"
+health="$(echo "$health" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g')" # trim whitespace (otherwise we'll have "green ")
+
+until [ "$health" = 'green' ]; do
+    health="$(curl -fsSL "$ES_HOST/_cat/health?h=status")"
+    health="$(echo "$health" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g')" # trim whitespace (otherwise we'll have "green ")
+    >&2 echo "Elastic Search is unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "Elastic Search is up"
---
+++ tools/wait_for_opensearch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+ES_HOST=${1:-"localhost:9200"}
+
+until $(curl --output /dev/null --silent --head --fail "$ES_HOST"); do
+    printf '.'
+    sleep 1
+done
