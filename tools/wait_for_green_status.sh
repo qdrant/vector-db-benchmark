9eb33501c9df915a58ac4692b1584130ba3b962d
tellet-q <elena.dubrovina@qdrant.com>
2024-04-18 09:42:55 +0200

    Avoid curl std output, use glove-25 for all engines

--- tools/wait_for_green_status.sh
+++ tools/wait_for_green_status.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
+# This scripts helps to wait for Opensearch|Elasticsearch status to become Green
 
 set -e
 
-ES_HOST=${1:-"localhost:9200"}
+SEARCH_CLUSTER_HOST=${1:-"localhost:9200"}
 
-until $(curl --output /dev/null --silent --head --fail "$ES_HOST"); do
+# Wait until the search cluster host is available
+until $(curl --output /dev/null --silent --head --fail "$SEARCH_CLUSTER_HOST"); do
     printf '.'
-    sleep 1
+    sleep 1 # Wait for 1 second
 done
 
-# Wait for ES/OS to start...
-response=$(curl "$ES_HOST")
+# Wait for ES/OS to start
+response=$(curl --write-out %{http_code} --silent --output /dev/null "$SEARCH_CLUSTER_HOST")
+
 until [ "$response" = "200" ]; do
-    response=$(curl --write-out %{http_code} --silent --output /dev/null "$ES_HOST")
-    >&2 echo "Search cluster is unavailable - sleeping"
+    response=$(curl --write-out %{http_code} --silent --output /dev/null "$SEARCH_CLUSTER_HOST")
+    >&2 echo "Search cluster is unavailable - sleep 1s"
     sleep 1
 done
 
 # Wait for ES/OS status to turn Green
-health="$(curl -fsSL "$ES_HOST/_cat/health?h=status")"
+health="$(curl -fsSL "$SEARCH_CLUSTER_HOST/_cat/health?h=status")"
 health="$(echo "$health" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g')"
 
 until [ "$health" = 'green' ]; do
-    health="$(curl -fsSL "$ES_HOST/_cat/health?h=status")"
+    health="$(curl -fsSL "$SEARCH_CLUSTER_HOST/_cat/health?h=status")"
     health="$(echo "$health" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g')"
-    >&2 echo "Search cluster is yet unavailable, sleep 1"
+    >&2 echo "Search cluster status is not green yet - sleep 1s"
     sleep 1
 done
 
