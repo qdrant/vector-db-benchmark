4585d0b548ad3c4342b652d6086eb7c3073d2aac
filipecosta90 <filipecosta.90@gmail.com>
2024-05-27 17:35:30 +0100

    Fixes per PR review: isort

--- engine/clients/mongodb/__init__.py
+++ engine/clients/mongodb/__init__.py
@@ -1,3 +1,4 @@
+# noqa: F401
 from engine.clients.mongodb.configure import MongoConfigurator
 from engine.clients.mongodb.search import MongoSearcher
 from engine.clients.mongodb.upload import MongoUploader
