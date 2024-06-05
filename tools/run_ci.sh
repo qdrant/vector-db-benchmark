14bd308bfadbd28f7aab81ba692d806695e63658
KShivendu <kshivendu1@gmail.com>
2024-06-05 14:43:48 +0530

    fix: Use quotes on func name to declare they arent commands

--- tools/run_ci.sh
+++ tools/run_ci.sh
@@ -12,8 +12,8 @@ function handle_term() {
   echo "::set-output name=failed::true"
 }
 
-trap handle_err ERR
-trap handle_term TERM
+trap 'handle_err' ERR
+trap 'handle_term' TERM
 
 # Script, that runs benchmark within the GitHub Actions CI environment
 
