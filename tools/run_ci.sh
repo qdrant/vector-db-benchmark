811d64a04c647e8890495f858623d1e1d9eefcac
KShivendu <kshivendu1@gmail.com>
2024-08-07 14:31:19 +0530

    More types of traps

--- tools/run_ci.sh
+++ tools/run_ci.sh
@@ -13,7 +13,7 @@ function handle_term() {
   echo "failed=timeout" >> $GITHUB_OUTPUT
 }
 
-trap 'handle_err' ERR
+trap 'handle_err' ERR INT EXIT
 trap 'handle_term' TERM
 
 # temporarily throw error intentionally:
