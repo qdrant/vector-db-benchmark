5cfdb0b9a307837ba08921397a6db2a281691f15
Kumar Shivendu <kshivendu1@gmail.com>
2024-08-07 14:34:29 +0530

    Fail CI if any benches fail (#180)
    * Fail CI if any benches fail
    
    * More types of traps
    
    * remove intentional exit

--- tools/run_ci.sh
+++ tools/run_ci.sh
@@ -5,15 +5,15 @@ set -euo pipefail
 
 function handle_err() {
   echo "Error occured qdrant_version=${QDRANT_VERSION} engine_name=${ENGINE_NAME} dataset=${DATASETS}"
-  echo "{failed}={error}" >> $GITHUB_OUTPUT
+  echo "failed=error" >> $GITHUB_OUTPUT
 }
 
 function handle_term() {
   echo "Timeout occured qdrant_version=${QDRANT_VERSION} engine_name=${ENGINE_NAME} dataset=${DATASETS}"
-  echo "{failed}={timeout}" >> $GITHUB_OUTPUT
+  echo "failed=timeout" >> $GITHUB_OUTPUT
 }
 
-trap 'handle_err' ERR
+trap 'handle_err' ERR INT EXIT
 trap 'handle_term' TERM
 
 # Script, that runs benchmark within the GitHub Actions CI environment
