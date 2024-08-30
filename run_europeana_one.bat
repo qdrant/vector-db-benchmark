f917d9b428c7e878b980a8e94afc25058863b8ed
gsergiu <4517853+gsergiu@users.noreply.github.com>
2024-08-30 14:31:47 +0200

    #1 switch to windows exp configurstions

--- run_europeana_one.bat
+++ run_europeana_one.bat
@@ -3,17 +3,17 @@ rem configure the ENGINE_NAME and the DATASETS with the wished values and run th
 rem NOTE: the database service needs to be run manually using the corresponding docker-compose command
 
 set DATASETS="random-100"
-rem all milvus m-16
-set ENGINE_NAME="milvusw-m-16-*"
-rem all redis m-16
-rem set ENGINE_NAME="redis-m-16-*"
+rem all milvusw m-16
+rem set ENGINE_NAME="milvusw-m-16-*"
+rem all redisw m-16
+set ENGINE_NAME="redisw-m-16-*"
 rem all qdrant m-16
-rem set ENGINE_NAME="qdrant-m-16-*"
+rem set ENGINE_NAME="qdrantw-m-16-*"
 rem all milvus
-rem ENGINE_NAME="milvus-m-*"
+rem ENGINE_NAME="milvusw-m-*"
 
 echo "Running experiment for engine(s): %ENGINE_NAME% using dataset: %DATASETS%"
-py run.py --engines "%ENGINE_NAME%" --datasets "%DATASETS%"
+python run.py --engines "%ENGINE_NAME%" --datasets "%DATASETS%"
 
 pause ..
 
