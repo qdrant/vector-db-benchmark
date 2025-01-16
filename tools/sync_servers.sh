a5eb0340788c7dc44e4323fc0cca72bc7d770725
tellet-q <elena.dubrovina@qdrant.com>
2025-01-16 10:10:46 +0100

    Revert "Add ServerAliveInterval and ServerAliveCountMax to rsync and more (#220)"
    This reverts commit c805233e3026e0d96f5a2bcfd510ad3188684f32.

--- tools/sync_servers.sh
+++ tools/sync_servers.sh
@@ -2,5 +2,5 @@
 
 PROJECT_PATH=$(realpath "$(dirname "$0")/..")
 
-rsync -e "ssh -o ServerAliveInterval=10 -o ServerAliveCountMax=10" -avP --mkpath\
+rsync -avP --mkpath\
    "$PROJECT_PATH/engine/servers/" $1:./projects/vector-db-benchmark/engine/servers/
