bf420654cc22725e05d0c32b7746ffa21ef7d32e
tellet-q <elena.dubrovina@qdrant.com>
2025-02-25 12:44:24 +0100

    Add workflow: trigger image build

--- tools/compare_versions/prepare_image.sh
+++ tools/compare_versions/prepare_image.sh
@@ -5,6 +5,13 @@ QDRANT_VERSION=${QDRANT_VERSION:-"ghcr/dev"}
 #MAX_RETRIES=12
 MAX_RETRIES=1
 
+EVENT_TYPE="benchmark-trigger-image-build"
+
+if [[ -z "${BEARER_TOKEN}" ]]; then
+  echo "BEARER_TOKEN is not set. Exiting."
+  exit 1
+fi
+
 # check if version starts with "docker" or "ghcr"
 if [[ ${QDRANT_VERSION} == docker/* ]] || [[ ${QDRANT_VERSION} == ghcr/* ]]; then
 
@@ -31,19 +38,32 @@ else
   echo "Image $IMAGE does not exist in the remote repository."
 fi
 
-# TODO: add logic to trigger image build in qdrant repo
+if [[ "${CONTAINER_REGISTRY}" == "docker.io" ]]; then
+  echo "Impossible to push the image to Docker Container Registry in this workflow."
+  exit 1
+fi
 
-echo "Waiting for the image to appear in the remote repository..."
+echo "Trigger image build for $IMAGE..."
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${BEARER_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/qdrant/qdrant/dispatches \
+  -d "{\"event_type\": \"$EVENT_TYPE\", \"client_payload\": {\"version\": \"$QDRANT_VERSION\"}}"
+
+echo "Wait for the image to appear in the remote repository..."
 counter=0
 while ! docker manifest inspect "$IMAGE" > /dev/null 2>&1; do
   if [ $counter -ge $MAX_RETRIES ]; then
     echo "Reached maximum retries. Exiting."
-    exit 1
+    exit 2
   fi
   # sleep for 10 minutes, in seconds
   # together with the MAX_RETRIES it
   # will wait for 120 minutes
-  sleep 600
+#  sleep 600
+  sleep 60
   ((counter++))
 done
 
