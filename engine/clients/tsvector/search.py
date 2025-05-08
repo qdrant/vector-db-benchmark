2d7477af2faece696f9a3bf1cb96d3b1d54f30e8
tellet-q <elena.dubrovina@qdrant.com>
2025-05-08 11:10:18 +0200

    Configure tsvector experiment (search only)

---
+++ engine/clients/tsvector/search.py
@@ -0,0 +1,83 @@
+from typing import List, Tuple
+
+import numpy as np
+import psycopg
+from pgvector.psycopg import register_vector
+
+from dataset_reader.base_reader import Query
+from engine.base_client.distances import Distance
+from engine.base_client.search import BaseSearcher
+from engine.clients.pgvector.config import get_db_config
+from engine.clients.pgvector.parser import PgVectorConditionParser
+
+
+CONNECTION_SETTINGS = [
+    "set work_mem = '2GB';",
+    "set maintenance_work_mem = '8GB';"
+    "set max_parallel_workers_per_gather = 0;",
+    "set enable_seqscan=0;",
+    "set jit = 'off';",
+]
+
+
+class PgVectorSearcher(BaseSearcher):
+    conn = None
+    cur = None
+    distance = None
+    search_params = {}
+    parser = PgVectorConditionParser()
+
+    @classmethod
+    def init_client(cls, host, distance, connection_params: dict, search_params: dict):
+        cls.distance = distance
+        cls.conn = psycopg.connect(**get_db_config(host, connection_params))
+        register_vector(cls.conn)
+        cls.cur = cls.conn.cursor()
+
+        cls.cur.execute("set diskann.query_search_list_size = %d" %
+                         connection_params['query_search_list_size'])
+        print("set diskann.query_search_list_size = %d" %
+                  connection_params['query_search_list_size'])
+        cls.cur.execute("set diskann.query_rescore = %d" %
+                         connection_params['query_rescore'])
+        print("set diskann.query_rescore = %d" % connection_params['query_rescore'])
+
+        for setting in CONNECTION_SETTINGS:
+            cls.cur.execute(setting)
+
+        print("Prewarming...")
+        cls.cur.execute(
+                "select format($$%I.%I$$, chunk_schema, chunk_name) from timescaledb_information.chunks k where hypertable_name = 'items'")
+        chunks = [row[0] for row in cls.cur]
+        for chunk in chunks:
+            print(f"prewarming chunk heap {chunk}")
+            cls.cur.execute(
+                f"select pg_prewarm('{chunk}'::regclass, mode=>'buffer')")
+            cls.cur.fetchall()
+
+        cls.cur.execute("""
+                select format($$%I.%I$$, x.schemaname, x.indexname)
+                from timescaledb_information.chunks k
+                inner join pg_catalog.pg_indexes x on (k.chunk_schema = x.schemaname and k.chunk_name = x.tablename)
+                where x.indexname ilike '%_embedding_%'
+                and k.hypertable_name = 'items'""")
+        chunks = [row[0] for row in cls.cur]
+        for chunk_index in chunks:
+            print(f"prewarming chunk index {chunk_index}")
+            cls.cur.execute(
+                f"select pg_prewarm('{chunk_index}'::regclass, mode=>'buffer')")
+            cls.cur.fetchall()
+
+    @classmethod
+    def search_one(cls, query: Query, top) -> List[Tuple[int, float]]:
+        # TODO: Use query.metaconditions for datasets with filtering
+        cls.cur.execute(
+            cls.query, (np.array(query.vector), top), binary=True, prepare=True
+        )
+        return cls.cur.fetchall()
+
+    @classmethod
+    def delete_client(cls):
+        if cls.cur:
+            cls.cur.close()
+            cls.conn.close()
---
+++ engine/clients/tsvector/upload.py
@@ -0,0 +1,43 @@
+from typing import List
+
+import numpy as np
+import psycopg
+from pgvector.psycopg import register_vector
+
+from dataset_reader.base_reader import Record
+from engine.base_client import IncompatibilityError
+from engine.base_client.distances import Distance
+from engine.base_client.upload import BaseUploader
+from engine.clients.pgvector.config import get_db_config
+
+
+class TsVectorUploader(BaseUploader):
+    DISTANCE_MAPPING = {
+        Distance.L2: "vector_l2_ops",
+        Distance.COSINE: "vector_cosine_ops",
+    }
+    conn = None
+    cur = None
+    upload_params = {}
+
+    @classmethod
+    def init_client(cls, host, distance, connection_params, upload_params):
+        cls.conn = psycopg.connect(**get_db_config(host, connection_params))
+        register_vector(cls.conn)
+        cls.cur = cls.conn.cursor()
+        cls.upload_params = upload_params
+
+    @classmethod
+    def upload_batch(cls, batch: List[Record]):
+        print("Data upload already done")
+
+    @classmethod
+    def post_upload(cls, distance):
+        print("Already done")
+        return {}
+
+    @classmethod
+    def delete_client(cls):
+        if cls.cur:
+            cls.cur.close()
+            cls.conn.close()
