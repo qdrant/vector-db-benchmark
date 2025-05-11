46f48853f958c9dc5a0b1661ab176a02f55fab30
tellet-q <elena.dubrovina@qdrant.com>
2025-05-11 13:00:29 +0200

    Fix config

--- engine/clients/tsvector/configure.py
+++ engine/clients/tsvector/configure.py
@@ -3,7 +3,7 @@ import psycopg
 
 from benchmark.dataset import Dataset
 from engine.base_client.configure import BaseConfigurator
-from engine.clients.pgvector.config import get_db_config
+from engine.clients.tsvector.config import get_db_config
 
 
 class TsVectorConfigurator(BaseConfigurator):
