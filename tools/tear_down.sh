f0f4ae27f29996c7a2c71983b308237aa6b0c0d0
tellet-q <elena.dubrovina@qdrant.com>
2025-06-05 12:45:52 +0200

    Create server and remove after run

--- tools/tear_down.sh
+++ tools/tear_down.sh
@@ -8,7 +8,7 @@ SCRIPT=$(realpath "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 
 BENCH_SERVER_NAME=${SERVER_NAME:-"benchmark-server-1"}
-BENCH_CLIENT_NAME=${CLIENT_NAME:-"benchmark-client-1"}
+#BENCH_CLIENT_NAME=${CLIENT_NAME:-"benchmark-client-1"}
 
 bash -x "${SCRIPT_PATH}/${CLOUD_NAME}/remove_server.sh" "$BENCH_SERVER_NAME"
-bash -x "${SCRIPT_PATH}/${CLOUD_NAME}/remove_server.sh" "$BENCH_CLIENT_NAME"
+#bash -x "${SCRIPT_PATH}/${CLOUD_NAME}/remove_server.sh" "$BENCH_CLIENT_NAME"
